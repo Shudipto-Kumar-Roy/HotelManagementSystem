f79d82ddfe0fc25a774a20c1ac3ef2ba670feae9
Naim <naimhasannwu@gmail.com>
2020-12-13 21:08:33 +0600

    update payment test file added

---
+++ app/src/test/java/com/example/hotelmanagementapplication/UpdatePaymentTest.java
@@ -0,0 +1,16 @@
+package com.example.hotelmanagementapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UpdatePaymentTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onClick() {
+    }
+}
