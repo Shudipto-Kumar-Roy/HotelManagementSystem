93354e437e17301f41c678c07703ac8e4c1d3cfa
Shudipto <shudipto345@gmail.com>
2020-12-20 16:54:52 +0600

    Delete guest onclick test file added

---
+++ app/src/test/java/com/example/hotelmanagementapplication/DeleteGuestOnclickTest.java
@@ -0,0 +1,16 @@
+package com.example.hotelmanagementapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DeleteGuestOnclickTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onClick() {
+    }
+}
