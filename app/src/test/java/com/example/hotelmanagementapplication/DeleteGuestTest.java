e6ca264030184709580cca8fa8aa4f5b37b44402
akash <akashbala1433@gmail.com>
2020-12-20 22:22:18 +0600

    delete guest test file added successfully

---
+++ app/src/test/java/com/example/hotelmanagementapplication/DeleteGuestTest.java
@@ -0,0 +1,24 @@
+package com.example.hotelmanagementapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DeleteGuestTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onClick() {
+    }
+
+    @Test
+    public void testOnCreate() {
+    }
+
+    @Test
+    public void testOnClick() {
+    }
+}
