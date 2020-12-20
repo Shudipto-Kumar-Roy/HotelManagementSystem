5ede7e7e9218d6c41f8d24c49c28ea12bf0de1c5
Shudipto <shudipto345@gmail.com>
2020-12-20 16:51:13 +0600

    All living guest loadData function test file added

---
+++ app/src/test/java/com/example/hotelmanagementapplication/AllLivingGuesLoadDataTest.java
@@ -0,0 +1,16 @@
+package com.example.hotelmanagementapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AllLivingGuesLoadDataTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void loadData() {
+    }
+}
